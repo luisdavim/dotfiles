45a3b1319b4321fac1e058c309e96f649705f32d
Luis Davim <luis.davim@broadcom.com>
2025-02-25 11:29:14 +0000

    update mini.nvim config

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -1005,12 +1005,12 @@ end)
 --   }
 -- end)
 
--- later(function()
---   add({
---     source = 'sindrets/diffview.nvim'
---   })
---   require("diffview").setup()
--- end)
+later(function()
+  add({
+    source = 'sindrets/diffview.nvim'
+  })
+  require("diffview").setup()
+end)
 
 later(function() require('mini.diff').setup() end)
 
@@ -1104,42 +1104,42 @@ end)
 --   snippets.setup({ snippets = { gen_loader.from_lang() } })
 -- end)
 
-later(function()
-  local miniclue = require('mini.clue')
-  --stylua: ignore
-  miniclue.setup({
-    clues = {
-      -- TODO: add custom mappings clues
-      miniclue.gen_clues.builtin_completion(),
-      miniclue.gen_clues.g(),
-      miniclue.gen_clues.marks(),
-      miniclue.gen_clues.registers(),
-      miniclue.gen_clues.windows({ submode_resize = true }),
-      miniclue.gen_clues.z(),
-    },
-    triggers = {
-      { mode = 'n', keys = '<Leader>' }, -- Leader triggers
-      { mode = 'x', keys = '<Leader>' },
-      { mode = 'n', keys = [[\]] },      -- mini.basics
-      { mode = 'n', keys = '[' },        -- mini.bracketed
-      { mode = 'n', keys = ']' },
-      { mode = 'x', keys = '[' },
-      { mode = 'x', keys = ']' },
-      { mode = 'i', keys = '<C-x>' }, -- Built-in completion
-      { mode = 'n', keys = 'g' },     -- `g` key
-      { mode = 'x', keys = 'g' },
-      { mode = 'n', keys = "'" },     -- Marks
-      { mode = 'n', keys = '`' },
-      { mode = 'x', keys = "'" },
-      { mode = 'x', keys = '`' },
-      { mode = 'n', keys = '"' }, -- Registers
-      { mode = 'x', keys = '"' },
-      { mode = 'i', keys = '<C-r>' },
-      { mode = 'c', keys = '<C-r>' },
-      { mode = 'n', keys = '<C-w>' }, -- Window commands
-      { mode = 'n', keys = 'z' },     -- `z` key
-      { mode = 'x', keys = 'z' },
-    },
-    window = { config = { border = 'double' } },
-  })
-end)
+-- later(function()
+--   local miniclue = require('mini.clue')
+--   --stylua: ignore
+--   miniclue.setup({
+--     clues = {
+--       -- TODO: add custom mappings clues
+--       miniclue.gen_clues.builtin_completion(),
+--       miniclue.gen_clues.g(),
+--       miniclue.gen_clues.marks(),
+--       miniclue.gen_clues.registers(),
+--       miniclue.gen_clues.windows({ submode_resize = true }),
+--       miniclue.gen_clues.z(),
+--     },
+--     triggers = {
+--       { mode = 'n', keys = '<Leader>' }, -- Leader triggers
+--       { mode = 'x', keys = '<Leader>' },
+--       { mode = 'n', keys = [[\]] },      -- mini.basics
+--       { mode = 'n', keys = '[' },        -- mini.bracketed
+--       { mode = 'n', keys = ']' },
+--       { mode = 'x', keys = '[' },
+--       { mode = 'x', keys = ']' },
+--       { mode = 'i', keys = '<C-x>' }, -- Built-in completion
+--       { mode = 'n', keys = 'g' },     -- `g` key
+--       { mode = 'x', keys = 'g' },
+--       { mode = 'n', keys = "'" },     -- Marks
+--       { mode = 'n', keys = '`' },
+--       { mode = 'x', keys = "'" },
+--       { mode = 'x', keys = '`' },
+--       { mode = 'n', keys = '"' }, -- Registers
+--       { mode = 'x', keys = '"' },
+--       { mode = 'i', keys = '<C-r>' },
+--       { mode = 'c', keys = '<C-r>' },
+--       { mode = 'n', keys = '<C-w>' }, -- Window commands
+--       { mode = 'n', keys = 'z' },     -- `z` key
+--       { mode = 'x', keys = 'z' },
+--     },
+--     window = { config = { border = 'double' } },
+--   })
+-- end)
