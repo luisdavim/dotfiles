13eabdb4e58a2ba5f11343fafa741ffcc2a6decd
Luis Davim <luis.davim@broadcom.com>
2025-05-07 16:26:05 +0100

    update nvim config

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -779,7 +779,7 @@ now(function()
   })
 end)
 
-later(function()
+now(function()
   add({
     source = 'tanvirtin/vgit.nvim',
     depends = {
@@ -931,37 +931,37 @@ later(function()
   -- use <Alt-e> to eval expressions
   keymap({ 'n', 'v' }, '<M-e>', function() require('dapui').eval() end)
 
-  vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    desc = "Prevent colorscheme clearing self-defined DAP marker colors",
-    callback = function()
-      -- Reuse current SignColumn background (except for DapStoppedLine)
-      local sign_column_hl = vim.api.nvim_get_hl(0, { name = 'SignColumn' })
-      -- if bg or ctermbg aren't found, use bg = 'bg' (which means current Normal) and ctermbg = 'Black'
-      -- convert to 6 digit hex value starting with #
-      local sign_column_bg = (sign_column_hl.bg ~= nil) and ('#%06x'):format(sign_column_hl.bg) or 'bg'
-      local sign_column_ctermbg = (sign_column_hl.ctermbg ~= nil) and sign_column_hl.ctermbg or 'Black'
-
-      vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#00ff00', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
-      vim.api.nvim_set_hl(0, 'DapStoppedLine', { bg = '#2e4d3d', ctermbg = 'Green' })
-      vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#c23127', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
-      vim.api.nvim_set_hl(0, 'DapBreakpointRejected',
-        { fg = '#888ca6', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
-      vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
-    end
-  })
-
-  vim.fn.sign_define('DapBreakpoint',
-    { text = '⏹', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-  vim.fn.sign_define('DapBreakpointCondition',
-    { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-  vim.fn.sign_define('DapBreakpointRejected',
-    { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-  vim.fn.sign_define('DapLogPoint', { text = '🗎', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
-  vim.fn.sign_define('DapStopped', { text = '⏵', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
-
-  -- reload current color scheme to pick up colors override if it was set up in a lazy plugin definition fashion
-  vim.cmd.colorscheme(vim.g.colors_name)
+  -- vim.api.nvim_create_autocmd("ColorScheme", {
+  --   pattern = "*",
+  --   desc = "Prevent colorscheme clearing self-defined DAP marker colors",
+  --   callback = function()
+  --     -- Reuse current SignColumn background (except for DapStoppedLine)
+  --     local sign_column_hl = vim.api.nvim_get_hl(0, { name = 'SignColumn' })
+  --     -- if bg or ctermbg aren't found, use bg = 'bg' (which means current Normal) and ctermbg = 'Black'
+  --     -- convert to 6 digit hex value starting with #
+  --     local sign_column_bg = (sign_column_hl.bg ~= nil) and ('#%06x'):format(sign_column_hl.bg) or 'bg'
+  --     local sign_column_ctermbg = (sign_column_hl.ctermbg ~= nil) and sign_column_hl.ctermbg or 'Black'
+  --
+  --     vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#00ff00', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+  --     vim.api.nvim_set_hl(0, 'DapStoppedLine', { bg = '#2e4d3d', ctermbg = 'Green' })
+  --     vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#c23127', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+  --     vim.api.nvim_set_hl(0, 'DapBreakpointRejected',
+  --       { fg = '#888ca6', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+  --     vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+  --   end
+  -- })
+  --
+  -- vim.fn.sign_define('DapBreakpoint',
+  --   { text = '⏹', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+  -- vim.fn.sign_define('DapBreakpointCondition',
+  --   { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+  -- vim.fn.sign_define('DapBreakpointRejected',
+  --   { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+  -- vim.fn.sign_define('DapLogPoint', { text = '🗎', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
+  -- vim.fn.sign_define('DapStopped', { text = '⏵', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+  --
+  -- -- reload current color scheme to pick up colors override if it was set up in a lazy plugin definition fashion
+  -- vim.cmd.colorscheme(vim.g.colors_name)
 
   keymap('n', '<F5>', dap.continue)
   keymap('n', '<F10>', dap.step_over)
