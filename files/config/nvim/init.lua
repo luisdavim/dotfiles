1609cbf9bf48c2e5a917c335aca83f5fea5d8bf4
Luis Davim <luis.davim@gmail.com>
2025-05-22 13:06:25 +0100

    update nvim config

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -166,185 +166,6 @@ now(function()
   })
 end)
 
--- Auto-completion
-
--- Sinppets
--- later(function()
---   local snippets = require('mini.snippets')
---   local gen_loader = snippets.gen_loader
---   snippets.setup({ snippets = { gen_loader.from_lang() } })
---   MiniSnippets.start_lsp_server()
--- end)
-
--- Using mini.completion
--- now(function()
---   require('mini.completion').setup({
---     window = {
---       info = { height = 25, width = 80, border = 'single' },
---       signature = { height = 25, width = 60, border = 'single' },
---     },
---   })
---   -- Override completeopt
---   vim.opt.completeopt = 'longest,menuone,noselect'
---   -- Use fuzzy matching for built-in completion
---   if vim.fn.has "nvim-0.11" == 1 then
---     vim.opt.completeopt:append "fuzzy"
---   end
---
---
---   local term = vim.api.nvim_replace_termcodes('<C-z>', true, true, true)
---   vim.opt.wildmenu = true
---   vim.opt.wildoptions = 'pum,fuzzy'
---   vim.opt.wildmode = 'noselect:lastused,full'
---   vim.opt.wildcharm = vim.fn.char2nr(term)
---
---   vim.keymap.set('c', '<Up>', '<End><C-U><Up>', { silent = true })
---   vim.keymap.set('c', '<Down>', '<End><C-U><Down>', { silent = true })
---
---   vim.api.nvim_create_autocmd('CmdlineChanged', {
---     pattern = ':',
---     callback = function()
---       local cmdline = vim.fn.getcmdline()
---       local curpos = vim.fn.getcmdpos()
---       local last_char = cmdline:sub(-1)
---
---       if
---           curpos == #cmdline + 1
---           and vim.fn.pumvisible() == 0
---           and last_char:match('[%w%/%: ]')
---           and not cmdline:match('^%d+$')
---       then
---         vim.opt.eventignore:append('CmdlineChanged')
---         vim.api.nvim_feedkeys(term, 'ti', false)
---         vim.schedule(function()
---           vim.opt.eventignore:remove('CmdlineChanged')
---         end)
---       end
---     end,
---   })
--- end)
-
--- Using nvim-cmp
-now(function()
-  add({
-    source = 'hrsh7th/nvim-cmp',
-    depends = {
-      'neovim/nvim-lspconfig',
-      'onsails/lspkind.nvim',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-emoji',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-vsnip',
-      'hrsh7th/vim-vsnip',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-nvim-lsp-document-symbol',
-      -- 'petertriho/cmp-git',
-    }
-  })
-  -- Set up nvim-cmp.
-  local cmp = require('cmp')
-  local lspkind = require('lspkind')
-
-  cmp.setup({
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-        -- For `mini.snippets` users:
-        -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-        -- insert({ body = args.body }) -- Insert at cursor
-        -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-        -- require("cmp.config").set_onetime({ sources = {} })
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    formatting = {
-      expandable_indicator = true,
-      format = lspkind.cmp_format({
-        mode = "symbol_text",
-        menu = ({
-          buffer = "[Buffer]",
-          nvim_lsp = "[LSP]",
-          luasnip = "[LuaSnip]",
-          nvim_lua = "[Lua]",
-          latex_symbols = "[Latex]",
-          path = "[Path]"
-        })
-      }),
-    },
-    completion = {
-      completeopt = 'menuone',
-    },
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'nvim_lsp_signature_help' },
-      { name = 'path' },
-      { name = 'emoji' },
-      { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = 'buffer' },
-    })
-  })
-
-  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-  -- Set configuration for specific filetype.
-  --[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]] --
-
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    completion = {
-      completeopt = 'menuone,noselect',
-    },
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp_document_symbol' },
-    }, {
-      { name = 'buffer' },
-    }),
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    completion = {
-      completeopt = 'menuone,noselect',
-    },
-    sources = cmp.config.sources({
-      { name = 'path' },
-    }, {
-      { name = 'cmdline' },
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-  })
-end)
-
 -- Snacks
 now(function()
   add({
@@ -430,6 +251,238 @@ now(function()
     end
   })
 end)
+
+-- Auto-completion
+
+-- Sinppets
+-- later(function()
+--   local snippets = require('mini.snippets')
+--   local gen_loader = snippets.gen_loader
+--   snippets.setup({ snippets = { gen_loader.from_lang() } })
+--   MiniSnippets.start_lsp_server()
+-- end)
+
+-- Using mini.completion
+-- now(function()
+--   require('mini.completion').setup({
+--     window = {
+--       info = { height = 25, width = 80, border = 'single' },
+--       signature = { height = 25, width = 60, border = 'single' },
+--     },
+--   })
+--   -- Override completeopt
+--   vim.opt.completeopt = 'longest,menuone,noselect'
+--   -- Use fuzzy matching for built-in completion
+--   if vim.fn.has "nvim-0.11" == 1 then
+--     vim.opt.completeopt:append "fuzzy"
+--   end
+--
+--
+--   local term = vim.api.nvim_replace_termcodes('<C-z>', true, true, true)
+--   vim.opt.wildmenu = true
+--   vim.opt.wildoptions = 'pum,fuzzy'
+--   vim.opt.wildmode = 'noselect:lastused,full'
+--   vim.opt.wildcharm = vim.fn.char2nr(term)
+--
+--   vim.keymap.set('c', '<Up>', '<End><C-U><Up>', { silent = true })
+--   vim.keymap.set('c', '<Down>', '<End><C-U><Down>', { silent = true })
+--
+--   vim.api.nvim_create_autocmd('CmdlineChanged', {
+--     pattern = ':',
+--     callback = function()
+--       local cmdline = vim.fn.getcmdline()
+--       local curpos = vim.fn.getcmdpos()
+--       local last_char = cmdline:sub(-1)
+--
+--       if
+--           curpos == #cmdline + 1
+--           and vim.fn.pumvisible() == 0
+--           and last_char:match('[%w%/%: ]')
+--           and not cmdline:match('^%d+$')
+--       then
+--         vim.opt.eventignore:append('CmdlineChanged')
+--         vim.api.nvim_feedkeys(term, 'ti', false)
+--         vim.schedule(function()
+--           vim.opt.eventignore:remove('CmdlineChanged')
+--         end)
+--       end
+--     end,
+--   })
+-- end)
+
+-- Using nvim-cmp
+now(function()
+  add({
+    source = 'hrsh7th/nvim-cmp',
+    depends = {
+      'neovim/nvim-lspconfig',
+      'onsails/lspkind.nvim',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-emoji',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-nvim-lsp-document-symbol',
+      -- 'petertriho/cmp-git',
+    }
+  })
+
+  -- AI autocomplete
+  -- add({
+  --   source = "zbirenbaum/copilot-cmp",
+  --   depends = { 'zbirenbaum/copilot.lua' },
+  -- })
+
+  -- Set up nvim-cmp.
+  local cmp = require('cmp')
+  local lspkind = require('lspkind')
+  -- require("copilot_cmp").setup()
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+        -- For `mini.snippets` users:
+        -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+        -- insert({ body = args.body }) -- Insert at cursor
+        -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+        -- require("cmp.config").set_onetime({ sources = {} })
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    formatting = {
+      expandable_indicator = true,
+      format = lspkind.cmp_format({
+        mode = "symbol_text",
+        menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[Latex]",
+          -- copilot = "[Copilot]",
+          path = "[Path]"
+        })
+      }),
+    },
+    completion = {
+      completeopt = 'menuone',
+    },
+    sources = cmp.config.sources({
+      -- { name = "copilot" },
+      { name = 'nvim_lsp' },
+      { name = 'nvim_lsp_signature_help' },
+      { name = 'path' },
+      { name = 'emoji' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+  -- Set configuration for specific filetype.
+  --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup() ]] --
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    completion = {
+      completeopt = 'menuone,noselect',
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp_document_symbol' },
+    }, {
+      { name = 'buffer' },
+    }),
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    completion = {
+      completeopt = 'menuone,noselect',
+    },
+    sources = cmp.config.sources({
+      { name = 'path' },
+    }, {
+      { name = 'cmdline' },
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
+end)
+
+-- AI
+-- now(function()
+--   -- CodeCompanion (for chat, agents, and integrated AI workflows)
+--   add({
+--     source = "olimorris/codecompanion.nvim",
+--     depends = {
+--       "nvim-lua/plenary.nvim",
+--       "nvim-treesitter/nvim-treesitter",
+--       "zbirenbaum/copilot.lua",
+--     },
+--   })
+--
+--   -- GitHub Copilot (for general inline completions)
+--   require("copilot").setup({
+--     suggestion = { enabled = false },
+--     panel = { enabled = false },
+--   })
+--   --NOTE: Set config path to enable the copilot adapter to work.
+--   --It will search the follwoing paths for the for copilot token:
+--   --  - "$CODECOMPANION_TOKEN_PATH/github-copilot/hosts.json"
+--   --  - "$CODECOMPANION_TOKEN_PATH/github-copilot/apps.json"
+--   vim.env["CODECOMPANION_TOKEN_PATH"] = vim.env["HOME"] .. "/.config"
+--
+--   require("codecompanion").setup({
+--     adapters = {
+--       copilot = function()
+--         return require("codecompanion.adapters").extend("copilot", {
+--           -- CodeCompanion will typically use the existing Copilot setup.
+--           -- No API key is usually needed here as Copilot uses GitHub authentication.
+--         })
+--       end,
+--     },
+--     strategies = {
+--       chat = { adapter = "copilot" },
+--       inline = { adapter = "copilot" },
+--       agent = { adapter = "copilot" },
+--     },
+--     opts = {
+--       log_level = "DEBUG",
+--     },
+--   })
+-- end)
 
 -- LSP config
 now(function()
